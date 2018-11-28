55a04c72deb19528c6b51d85ea0c8c2764da46a1
mmb566 <45395422+mmb566@users.noreply.github.com>
2018-11-28 19:03:47 +0100

    Codering

---
+++ Mustafa/Vuurwerkshop/Codering/Hoofd.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Vuurwerkshop</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+
+<div class="navigatie">
+	<nav>
+		
+	</nav>
+</div>
+
+
+
+
+
+</body>
+</html>
